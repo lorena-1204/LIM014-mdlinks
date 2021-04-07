e2d42c5486426d2baffd8aa70a6988a6c54aa177
lorena-1204 <mlrc1204@gmail.com>
2021-04-07 11:09:09 -0500

    Ruta relativa o absoluta.close #7

---
+++ src/api-index.js
@@ -0,0 +1,12 @@
+// path-> proporciona utilidades para trabajar con rutas de archivos y directorios .
+const path = require('path');
+
+// fs-> proporciona una API para interactuar con los ficheros y directorios
+const fs = require('fs');
+
+// Utiliza las solicitudes http (de forma asincrona)-solo peticiones a una API-rest,traer datos
+// const axios = require('axios');
+
+// Rutas relativas y absolutas
+const dirRelative = 'pruebas';
+// const fileAbsolute = 'C:\\Users\\Lorena RC\\Desktop\\LIM014-mdlinks\\pruebas\\prueba.md';
